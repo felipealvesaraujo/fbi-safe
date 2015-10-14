ec7012206b3839192d76c0f7a272bf747f702c02
avanti03 <avanti03@Mac-mini-de-avanti03.local>
2015-10-14 16:40:59 -0300

    Now the file is empty

--- functions.php
+++
@@ -1,9 +0,0 @@
-<?php  
-     
-    // ADICIONANDO STYLE SHEET NAS PÁGINAS 
-    function styleSheetFbiSafe(){
-        wp_enqueue_style('material.min', get_stylesheet_uri());
-    }
-    add_action('wp_enqueue_scripts', 'styleSheetFbiSafe');
-
- ?>
